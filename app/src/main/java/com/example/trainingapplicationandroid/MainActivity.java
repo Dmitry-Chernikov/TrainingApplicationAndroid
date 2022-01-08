bacf19f55049c4d4965adeba47294d7c9a2f0863
Dmitry-Chernikov <chernicov_dima@mail.ru>
2022-01-08 21:53:05 +0400

    Sizing elements in ConstraintLayout Размеры элементов в ConstraintLayout

--- app/src/main/java/com/example/trainingapplicationandroid/MainActivity.java
+++ app/src/main/java/com/example/trainingapplicationandroid/MainActivity.java
@@ -9,6 +9,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main_size_element);
+        //setContentView(R.layout.activity_main_stretching);
+        //setContentView(R.layout.activity_main_min_max);
+        //setContentView(R.layout.activity_main_size_percent);
+        //setContentView(R.layout.activity_main_proportion);
+        setContentView(R.layout.activity_main_ratio);
     }
 }
